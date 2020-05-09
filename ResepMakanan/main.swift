e29070f99aec5cf04d30bf1f7f2827372442e72d
fikri <fikrihelmi17@gmail.com>
2020-05-09 10:36:34 +0700

    Initial Commit

---
+++ ResepMakanan/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ResepMakanan
+//
+//  Created by Fikri on 09/05/20.
+//  Copyright © 2020 Dicoding Indonesia. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
